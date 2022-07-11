2083ec0f8a8fb96109410fc829190cf7a81f1be9
Joe Borrow <jmborrow@gmail.com>
2022-07-11 17:01:21 -0400

    npm and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      template: './client/index.html',
+    }
+  ),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    compress: true,
+    port: 8080,
+  },
+};
