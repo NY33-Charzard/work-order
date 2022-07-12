949de65f9508f8e07be764392bb6a9d93e5b5abb
AaronDreyfuss <AaronDreyfuss@gmail.com>
2022-07-12 11:08:09 -0400

    Added a method to close an order

--- server/controllers/workOrderController.js
+++ server/controllers/workOrderController.js
@@ -35,4 +35,21 @@ workOrderController.closedOrders = async (req, res, next) => {
   }
 }
 
+workOrderController.closeOrder = async (req, res, next) => {
+  const value = [req.params.id];
+  const query = 'UPDATE orders SET open = false WHERE _id = $1'
+  try {
+    const data = await db.query(query, value);
+    // this will just be an empty array
+    res.locals.closeOrder = data.rows;
+    return next();
+  } catch (err) {
+    next({
+      log: `Error ocurred in openOrders query controller: ${err}`,
+      status: 400,
+      message: { err: 'An error ocurred in the openOrders query controller'},
+    })
+  }
+}
+
 module.exports = workOrderController;
